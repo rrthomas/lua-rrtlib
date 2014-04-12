08ec09eb082dbbf8dcd401efb47ff7bb72875bc9
Reuben Thomas <rrt@sc3d.org>
2014-04-12 19:29:53 +0100

    Add csv.lua

---
+++ rrt/csv.lua
@@ -0,0 +1,48 @@
+require "std"
+
+-- Convert to/from CSV.
+-- To include the separator in a string, enclose it in double quotes.
+-- To escape a double-quote character, repeat it.
+
+function CSVToTable (l, sep)
+  local t = {}
+  local i = 1
+  sep = sep or ','
+  while i <= #l do
+    local c = l[i]
+    if c == '"' then -- quoted value
+      local s = ""
+      repeat
+        local from, to = l:find ('^%b""', i)
+        s = s .. l:sub (from + 1, to - 1)
+        i = to + 1
+        c = l[i]
+        if c == '"' then s = s .. '"' end
+        -- If the next character after the closing quote is another
+        -- quote, we have an escaped quote.
+      until c ~= '"'
+      table.insert (t, s)
+      assert (c == sep or c == "")
+      i = i + 1
+    else -- no quote, so look for the first separator
+      local from, to = l:find (sep, i)
+      if from then
+        table.insert (t, l:sub (i, from - 1))
+        i = to + 1
+      else -- no separator: use rest of string
+        table.insert (t, l:sub (i))
+        i = #l + 1
+      end
+    end
+  end
+  return t
+end
+
+-- FIXME: Don't insert redundant quotes (so we get closer to idempotent round-trip)
+function tableToCSV (t, sep)
+  return table.concat (list.map (function (s)
+                                   return '"' .. s:gsub ('"', '""') .. '"'
+                                 end,
+                                 t),
+                       sep or ',')
+end
